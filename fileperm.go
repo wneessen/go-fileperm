fa56cf7fae30eb2fb61449ff28d1200d862bb7e6
Winni Neessen <wn@neessen.net>
2021-07-22 15:40:10 +0200

    That was much more complicated than expected but should work properly now.
    Thanks to https://pirivan.gitlab.io/post/how-to-retrieve-file-ownership-information-in-golang/ for the inspriration on the user handling

--- fileperm.go
+++ fileperm.go
@@ -1,9 +1,14 @@
+// +build !windows,!plan9
 package fileperm
 
 import (
 	"os"
+	"os/user"
+	"strconv"
+	"syscall"
 )
 
+// Bitmask to represent different access level of the file in question
 const (
 	OS_READ        = 04
 	OS_WRITE       = 02
@@ -37,48 +42,135 @@ const (
 	OS_ALL_RWX = OS_ALL_RW | OS_GROUP_X
 )
 
-// UserWritable returns true if the filepath is writable by the current user
-func UserWritable(f string) bool {
-	fs, err := os.Lstat(f)
-	if err != nil {
-		return false
+// FilePermUser implements the main struct of the fileperm packages. All methods are based on it
+type FilePermUser struct {
+	Path        string
+	Stat        os.FileInfo
+	Uid         uint32
+	Gid         uint32
+	CurUserUid  int64
+	CurUserGids []int64
+}
+
+// NewFileUserPerm returns a new FilePermUser struct. NewFileUserPerm expects a file path string
+// as input and will return an error if the initial operations failed
+func NewFileUserPerm(f string) (FilePermUser, error) {
+	fpuObj := FilePermUser{Path: f}
+	fs, err := os.Lstat(fpuObj.Path)
+	if fs == nil {
+		return fpuObj, err
 	}
-	return fs.Mode().Perm()&OS_USER_W != 0
+	fpuObj.Stat = fs
+
+	if err := fpuObj.getUidGid(); err != nil {
+		return fpuObj, err
+	}
+
+	if err := fpuObj.getCurUserIds(); err != nil {
+		return fpuObj, err
+	}
+	return fpuObj, nil
+}
+
+// UserWritable returns true if the filepath is writable by the current user
+func (f *FilePermUser) UserWritable() bool {
+	if int64(f.Uid) == f.CurUserUid {
+		return f.Stat.Mode().Perm()&OS_USER_W != 0
+	}
+	for _, gId := range f.CurUserGids {
+		if int64(f.Gid) == gId {
+			return f.Stat.Mode().Perm()&OS_GROUP_W != 0
+		}
+	}
+	return f.Stat.Mode().Perm()&OS_OTH_W != 0
 }
 
 // UserReadable returns true if the filepath is readable by the current user
-func UserReadable(f string) bool {
-	fs, err := os.Lstat(f)
-	if err != nil {
-		return false
+func (f *FilePermUser) UserReadable() bool {
+	if int64(f.Uid) == f.CurUserUid {
+		return f.Stat.Mode().Perm()&OS_USER_R != 0
 	}
-	return fs.Mode().Perm()&OS_USER_R != 0
+	for _, gId := range f.CurUserGids {
+		if int64(f.Gid) == gId {
+			return f.Stat.Mode().Perm()&OS_GROUP_R != 0
+		}
+	}
+	return f.Stat.Mode().Perm()&OS_OTH_R != 0
 }
 
 // UserExecutable returns true if the filepath is executable by the current user
-func UserExecutable(f string) bool {
-	fs, err := os.Lstat(f)
-	if err != nil {
-		return false
+func (f *FilePermUser) UserExecutable() bool {
+	if int64(f.Uid) == f.CurUserUid {
+		return f.Stat.Mode().Perm()&OS_USER_X != 0
 	}
-	return fs.Mode().Perm()&OS_USER_X != 0
+	for _, gId := range f.CurUserGids {
+		if int64(f.Gid) == gId {
+			return f.Stat.Mode().Perm()&OS_GROUP_X != 0
+		}
+	}
+	return f.Stat.Mode().Perm()&OS_OTH_X != 0
 }
 
 // UserWriteReadable returns true if the filepath is write- and readable by the current user
-func UserWriteReadable(f string) bool {
-	fs, err := os.Lstat(f)
-	if err != nil {
-		return false
+func (f *FilePermUser) UserWriteReadable() bool {
+	if int64(f.Uid) == f.CurUserUid {
+		return f.Stat.Mode().Perm()&OS_USER_RW != 0
 	}
-	return fs.Mode().Perm()&OS_USER_RW != 0
+	for _, gId := range f.CurUserGids {
+		if int64(f.Gid) == gId {
+			return f.Stat.Mode().Perm()&OS_GROUP_RW != 0
+		}
+	}
+	return f.Stat.Mode().Perm()&OS_OTH_RW != 0
 }
 
 // UserWriteReadExecutable returns true if the filepath is write- and read- and executable by the
 // current user
-func UserWriteReadExecutable(f string) bool {
-	fs, err := os.Lstat(f)
-	if err != nil {
-		return false
+func (f *FilePermUser) UserWriteReadExecutable() bool {
+	if int64(f.Uid) == f.CurUserUid {
+		return f.Stat.Mode().Perm()&OS_USER_RWX != 0
 	}
-	return fs.Mode().Perm()&OS_USER_RWX != 0
+	for _, gId := range f.CurUserGids {
+		if int64(f.Gid) == gId {
+			return f.Stat.Mode().Perm()&OS_GROUP_RWX != 0
+		}
+	}
+	return f.Stat.Mode().Perm()&OS_OTH_RWX != 0
+}
+
+// getUidGid retrieves the owner id and group id of the current FilePermUser structs file
+func (f *FilePermUser) getUidGid() error {
+	fsSys := f.Stat.Sys().(*syscall.Stat_t)
+	if fsSys != nil {
+		f.Uid = fsSys.Uid
+		f.Gid = fsSys.Gid
+	}
+	return nil
+}
+
+// getCurUserIds retrieves the userid and groupids of the processes current user and stores them
+// in the FilePermUser struct
+func (f *FilePermUser) getCurUserIds() error {
+	curUser, err := user.Current()
+	if err != nil {
+		return err
+	}
+	uidInt, err := strconv.ParseInt(curUser.Uid, 10, 32)
+	if err != nil {
+		return err
+	}
+	f.CurUserUid = uidInt
+
+	userGroups, err := curUser.GroupIds()
+	if err != nil {
+		return err
+	}
+	for _, gid := range userGroups {
+		gidInt, err := strconv.ParseInt(gid, 10, 32)
+		if err != nil {
+			return err
+		}
+		f.CurUserGids = append(f.CurUserGids, gidInt)
+	}
+	return nil
 }
